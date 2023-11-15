04b8d7c455385651676afc84b6fac5f7fb15400a
ariztegui8 <jorgeariztegui8@gmail.com>
2023-11-14 22:10:34 -0300

    tuto movile

--- App.js
+++ App.js
@@ -1,30 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Pressable, Modal, Alert, SafeAreaView } from 'react-native';
+import caballo from './assets/caballos.jpg'
+import { useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import Test from './component/Test';
+import Card from './component/Card';
+
+
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false)
   return (
-    <View style={styles.container}>
-      <Text>Hace mate bobo!!! </Text>
-      <View 
-        style={{backgroundColor: 'red', width: '100%'}}
-      >
-      <ScrollView>
-      <Image
-        
-        source={require('./assets/caballos.jpg')}
-      />
-      </ScrollView>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <>
+
+      <SafeAreaView >
+        <View style={{ backgroundColor: 'aqua', padding: 10 }}>
+        <ScrollView>
+          <Text >Hola Jorgito
+            <Text style={{ color: '#fff' }}> GATO</Text>
+          </Text>
+          <Image source={caballo} style={{ width: 300, height: 300, objectFit: 'contain' }} />
+          <Pressable onPress={() => console.log('Text press')}>
+            <Text>¿Qué es Lorem Ipsum?
+              Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
+
+              ¿Por qué lo usamos?
+              Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).</Text>
+          </Pressable>
+          <Pressable onPress={() => console.log('Image press')}>
+            <Image source={{ uri: "https://www.mundodeportivo.com/alfabeta/hero/2023/09/goku-colores.jpg?width=1200&aspect_ratio=16:9" }} style={{ width: 300, height: 300, objectFit: 'contain' }} />
+          </Pressable>
+          <Button title='Button' color={'red'} onPress={() => setModalVisible(true)} />
+          <ActivityIndicator />
+
+          <Button title='Alert' onPress={() => Alert.alert('Invalid key', 'error al poner la key', [
+            {
+              text: 'Cancel',
+              onPress: () => { console.log('Cancel press'); }
+            },
+            {
+              text: 'Ok',
+              onPress: () => { console.log('Ok press'); }
+            }
+          ])} />
+
+         
+
+          <Test name="Goku" />
+          <Test name="Vegeta" />
+
+          <View style={{flex: 1, backgroundColor: '#fafafa'}}>
+            <View style={styles.boxColor1}></View>
+            <View style={styles.boxColor2}></View>
+          </View>
+
+          <Card />
+
+
+
+          <Modal visible={modalVisible}>
+            <View style={{ backgroundColor: 'skyblue', flex: 1, padding: 60 }}>
+              <Text>Modal React Native</Text>
+              <Button title='Close' color={'red'} onPress={() => setModalVisible(false)} />
+            </View>
+          </Modal>
+        </ScrollView>
+        </View>
+      </SafeAreaView>
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  boxColor1: {
+    backgroundColor: 'green',
+    borderWidth: 2,
+    borderColor: 'purple',
+    width: 250,
+    height: 250,
+    marginVertical: 20
   },
+  boxColor2: {
+    backgroundColor: 'red',
+    borderWidth: 2,
+    borderColor: 'purple',
+    width: 250,
+    height: 250,
+    marginVertical: 20
+  }
 });
---
+++ component/Card.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image, ScrollView, Button, Pressable, Modal, Alert } from 'react-native';
+import poke from '../assets/poke.webp'
+import corazon from '../assets/corazon.png'
+import fire from '../assets/fire.png'
+
+const Card = () => {
+    return (
+        <>
+        <View style={styles.box}>
+            <View style={{ gap: 2, flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30 }}>
+                <Text style={{ fontSize: 20, fontWeight: '600' }}>Squirtle</Text>
+
+                <View style={{ gap: 4, flexDirection: 'row', alignItems: 'center' }}>
+                    <Image source={corazon} style={{ width: 20, height: 20, objectFit: 'contain' }} />
+                    <Text style={{ fontWeight: 600 }}>39</Text>
+                </View>
+            </View>
+            <View style={{ justifyContent: 'center', marginBottom: 20 }}>
+                <Image source={poke} resizeMode='contain' style={{ width: '100%', height: 200 }} />
+            </View>
+
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <View style={{ gap: 8, flexDirection: 'row', borderRadius: 15, alignItems: 'center' , borderWidth: 2, borderColor: 'red', padding: 10, marginBottom: 30, justifyContent: 'center' ,  paddingHorizontal: 20}}>
+                    <Image source={fire} style={{ width: 20, height: 20, objectFit: 'contain' }} />
+                    <Text style={{ fontWeight: 600 }}>Fire</Text>
+                </View>
+            </View>
+
+            <View>
+                <Text style={{ fontWeight: '600', marginBottom: 10 }}>Squirtle es el mas rapido del oeste, se la banca, pega, y es guapo</Text>
+                <Text style={{ fontWeight: '600', marginBottom: 10 }}>Cuenta con poderes de fuego</Text>
+            </View>
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    box: {
+        backgroundColor: '#fafafa',
+        borderWidth: 2,
+        borderColor: '#000',
+        borderRadius: 10,
+        marginVertical: 20,
+        padding: 15,
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        flex: 1,
+        gap: 15
+    },
+
+});
+
+export default Card
